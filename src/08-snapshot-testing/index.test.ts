ee24c0698bcaba01fcab5581dd90ac21b0c21b5c
a-reznikov <aliaksandr.reznikau@pargoo.com>
2024-02-12 22:57:46 +0300

    feat: implement tests for 08-snapshot-testing module

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,45 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
+
+const linkedList1 = {
+  next: {
+    next: {
+      next: {
+        next: null,
+        value: null,
+      },
+      value: 3,
+    },
+    value: 2,
+  },
+  value: 1,
+};
+
+const linkedList2 = {
+  next: {
+    next: {
+      next: {
+        next: null,
+        value: null,
+      },
+      value: 'C',
+    },
+    value: 'B',
+  },
+  value: 'A',
+};
+
+const elements1 = [1, 2, 3];
+const elements2 = ['A', 'B', 'C'];
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(generateLinkedList(elements1)).toStrictEqual(linkedList1);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(generateLinkedList(elements2)).toMatchSnapshot(linkedList2);
   });
 });
